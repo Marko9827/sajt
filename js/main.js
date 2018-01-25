6a6e3c2f39715a0905da00356452a0377cf63ec3
Marko Nikolic <marko.supergun@gmail.com>
2018-01-25 14:59:30 +0100

    fffa

--- js/main.js
+++ js/main.js
@@ -4,6 +4,22 @@ document.addEventListener("dragstart", function (e) { e.preventDefault() }, fals
 document.addEventListener("selectstart", function (e) { e.preventDefault() }, false);
 
 
+
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 40) {
+        document.getElementById("myBtn").style.display = "block";
+       
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+function topFunction() {
+    document.body.scrollTop -= 1;
+    document.documentElement.scrollTop = 0;
+}
+
 ////km
 
 
@@ -14,31 +30,24 @@ function myFunction() {
     } else {
         x.className = "Meni_mobilni";
     }
-}  
-
-//var name = document.getElementById("name");
-//var email = document.getElementById("email");
-//var message = document.getElementById("message");
-
-var namee = document.getElementById("name");
-if (namee.innerHTML = "")
-{
-    btn.disabled =  true;
 }
+
+document.getElementById("indeks").innerHTML = "Marko Nikolić - MN2017200219IR";
+
+
 document.getElementById("hvala").style.display = "none";
 
 function btn() {
-    var dugme = document.getElementById("button");       var lb1 = document.getElementById("Lb1");
-    var name_F = document.getElementById("name");        var lb2 = document.getElementById("Lb2");
-    var email_F = document.getElementById("email");      var lb3 = document.getElementById("Lb3");
-    var message_F = document.getElementById("message");  var hvl = document.getElementById("hvala");
+    var dugme = document.getElementById("button"); var lb1 = document.getElementById("Lb1");
+    var name_F = document.getElementById("name"); var lb2 = document.getElementById("Lb2");
+    var email_F = document.getElementById("email"); var lb3 = document.getElementById("Lb3");
+    var message_F = document.getElementById("message"); var hvl = document.getElementById("hvala");
 
- 
 
-    if (name_F.innerHTML == "")
-     {
-            lb1.style.color = "#ff3333";
-            name_F.style = "border-left: 4px solid #ff3333;";    
+
+    if (name_F.innerHTML == "") {
+        lb1.style.color = "#ff3333";
+        name_F.style = "border-left: 4px solid #ff3333;";
     }
     if (email_F.innerHTML == "") {
         lb2.style.color = "#ff3333";
@@ -51,27 +60,15 @@ function btn() {
 
     }
 
-   
-       
-}
 
-function btn1() {
-    function toggleFullScreen() {
-        if (!document.fullscreenElement) {
-            document.documentElement.requestFullscreen();
-        } else {
-            if (document.exitFullscreen) {
-                document.exitFullscreen();
-            }
-        }
-    }
+
 }
 
 
 
 
 
-
+//Sve
 
 function openNav() {
     //document.getElementById("myNav").style.width = "100%";
@@ -80,10 +77,14 @@ function openNav() {
     document.getElementsByClassName("footer").style.marginLeft = "250px";
 
     document.getElementById("body").style = "overflow-y: hidden";
+
+    //greskamoguce
+    document.getElementsByClassName("Sve").style.marginLeft = "250px";
+
 }
 
 function closeNav() {
- //   document.getElementById("myNav").style.width = "0%";
+    //   document.getElementById("myNav").style.width = "0%";
     document.getElementById("myNav").style.width = "0%";
     document.getElementById("Sve").style.marginLeft = "0px";
 
@@ -98,21 +99,19 @@ function closeNav() {
 
 
 function onclick_yt() {
-    window.location.href = 'https://www.youtube.com/SuperKuazar/';
-    target = "_blank";
-
+    window.open('https://www.youtube.com/SuperKuazar/', '_blank'); 
 
 }
 
 
 
 function onclick_in() {
-    window.location.href = 'https://instagram.com/nikoliccc02/';
+    window.open('https://instagram.com/nikoliccc02/', '_blank');
 }
 
 function onclick_Go1() {
     window.location.href = 'https://marko9827.github.io/sajt/pages/page1.html';
- 
+
 
 }
 
@@ -137,10 +136,5 @@ function onclick_Go4() {
 
 }
 
-// _Pages
 
 
-/* _ PLUGIN SA MATIČNE STRANICE https://eronelit.com _ */
-/* _*/
-/* _*/
-/* _ PLUGIN SA MATIČNE STRANICE https://eronelit.com _ */
